401171bb34e0b86a60cb7f5a8baa39feef603484
Yuot-Cikom14719 <67092994+Yuot-Cikom14719@users.noreply.github.com>
2023-03-01 14:38:48 +0530

    CloudCart_Project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div>
+     <header>
+      <a href="home">CloudCart</a>
+     </header>
+     <main>
+      <ul>
+        <li>Products</li>
+      </ul>
+     </main>
+    </div>
+  );
+}
+
+export default App;
